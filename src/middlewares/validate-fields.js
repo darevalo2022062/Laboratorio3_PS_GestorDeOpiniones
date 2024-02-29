d84314e85bd21d205d0a8574cacdd186f8a1410b
David <darevalo-2022062@kinal.edu.gt>
2024-02-29 16:27:22 -0600

    Soluciónde error en middleware

--- src/middlewares/validate-fields.js
+++ src/middlewares/validate-fields.js
@@ -1,11 +1,10 @@
 import { validationResult } from "express-validator";
 
 export const validar = (req, res, next) => {
-    var error = validationResult(req);
-    if (!error.isEmpty()) {
+    var errors = validationResult(req);
+    if (!errors.isEmpty()) {
         const errorMsgs = errors.array().map(error => error.msg);
         return res.status(400).json({ errors: errorMsgs });
     }
-
     next();
 }
